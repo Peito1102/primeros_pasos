9d84aead626cef466a6c3d143310b9f69da1eedc
Peito1102 <renzovasquezch@hotmail.com>
2025-12-29 02:16:14 -0500

    subiendo mi primera practica de react

---
+++ src/FirstStepsApp.tsx
@@ -0,0 +1,14 @@
+export function FirstStepsApp() {
+    return(
+        <>
+        <h1>Heli, Mundo!</h1>
+        <p>Mi primero parrafo</p>
+
+        <button>Presioname</button>
+
+        <div>
+            <h2>Hola, estoy dentro de un div</h2>
+        </div>
+        </>
+    )
+}
---
+++ src/MyAwesomeApp.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const address = {
+    zipCode : '15074',
+    country : 'Perú'
+};
+const firstName = 'Renzo';
+const lastName = 'Vasquez';
+const favoriteGames = ['Zelda', 'Mario', 'Metroid'];
+
+export function MyAwesomeApp() {
+
+    
+    const [isActive, setIsActive] = useState(true);
+    
+
+    return (
+        <>
+        <hr />
+        <h1>{firstName}</h1>
+        <h3>{lastName}</h3>
+        <ul>
+            {favoriteGames.map((game) => (
+            
+            <li>{game}</li>
+        ))}
+        </ul>
+        <div>
+            <p>{isActive ? "Estoy activo" : "Ahora ya no"}</p>
+        </div>
+        
+        <div>
+            <p>{address.zipCode} - {address.country}</p> 
+            {/*<p>{Json.stringify(address)}</p> es otra forma de mostrar*/}
+        </div>
+
+        <button onClick={() => setIsActive(!isActive)}>{isActive ? "Desactivar" : "Activar"}</button>
+
+        </>
+    )
+}
